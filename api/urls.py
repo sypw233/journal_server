f3d00710ee70d70f068b129b2761d2ab0b6f551f
sypw <wumxing@live.com>
2025-04-25 14:21:49 +0800

    chore: 删除项目中的冗余文件和目录
    清理项目中不再需要的文件和目录，包括API、账户、同步API、日志应用等相关文件，以及数据库和IDE配置文件。此次清理旨在简化项目结构，移除不再使用的代码和配置。

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('journal-entries/', views.journal_entry_list, name='journal_entry_list'),
-    path('journal-entries/<int:pk>/', views.journal_entry_detail, name='journal_entry_detail'),
-    path('categories/', views.category_list, name='category_list'),
-    path('categories/<int:pk>/', views.category_detail, name='category_detail'),
-]
